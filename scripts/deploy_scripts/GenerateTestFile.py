ee07480204ba85a3474544b9d3d94aac53d09701
Bo5 Zhang <bo5.zhang@sonymobile.com>
2013-12-09 17:25:36 +0800

    Change the format of Python scripts.
    Change the format of Python scripts.
    
    Change-Id: Icb226145e81036bf648982c6f0637fec579e7a42

--- scripts/deploy_scripts/GenerateTestFile.py
+++ scripts/deploy_scripts/GenerateTestFile.py
@@ -8,12 +8,3 @@ def generateTestCaseFile(docTempdir, changeLogFile,changeLogDir):
 	Utile.writeLog("生成当前测试列表");
 	Utile.deploymentPlan_GenerateTableAction(ConstantValue.CHANGELOG_CURRENT_TESTCASE_TAG,ConstantValue.CHANGELOG_CURRENT_TESTCASE_TAG_END, docTempdir+ConstantValue.DEPLOYMENT_CURRENT_TESTCASE_FILENAME, changeLogFile) 
 	Utile.insertFile(ConstantValue.DEPLOMENT_RSTFILE_PLAYNOW_TOTALESTCASE_TAG,docTempdir+ConstantValue.DEPLOYMENT_BASE_TESTCASE_FILENAME,changeLogDir+ConstantValue.BASE_TESTCASE_FILENAME);
-    
-	
-	
-	
-
-	
-	
-	
-	
