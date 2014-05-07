95e2e6a68887301e6892d38669a0279ffd7e2910
Jingzhi Hu <jingzhi.hu@sonymobile.com>
2014-05-07 16:44:12 +0800

    add conflict
    add conflict
    
    Change-Id: Ic112961e51fa12cdbb2c0a9848dc7cdbd54132bb

--- 05_cloud/01_SonySales/Code/sonysales/src/com/sonymobile/sonysales/dao/HandleDAO.java
+++ 05_cloud/01_SonySales/Code/sonysales/src/com/sonymobile/sonysales/dao/HandleDAO.java
@@ -7,6 +7,22 @@ import com.sonymobile.sonysales.model.Handle;
 import com.sonymobile.sonysales.model.HibernateUtil;
 
 public class HandleDAO {
+
+        //获得前limit名获得拉手支持最多的用户昵称和支持者数量
+    public String getSupporterCount(String limit) {
+        String sql = "select u.nickname, l.count from user u,"+
+        "(select ownerUserId, count(supporterUserId) count from handle group by ownerUserId order by count desc limit "+limit+") l"+
+        " where u.id = l.ownerUserId";
+        return BaseDAO.findBySql(sql).toString();
+    }
+    
+        //根据openID查询拉友支持列表
+    public String getSupporters(String openId) {
+        String sql = "select h1.nickname,h1.createTime from user u1, (select u.nickname, h.createTime,h.ownerUserId from handle h, user u where u.id=h.supporterUserId ) h1"+
+        " where  h1.ownerUserId=u1.id and u1.openId="+openId;
+        return BaseDAO.findBySql(sql).toString();
+    }
+
 	public static boolean addHandle(Handle handle) {
 		HibernateUtil hibernateUtil = new HibernateUtil();
 		return hibernateUtil.addObject(handle);
